1b7dcd867f75b6ea51e48be62cf39b60fbf1e86e
emily <emily@bacheconsulting.com>
2022-05-20 13:34:54 +0200

    Added translation of Java tests

--- csharp/RPG-Combat/PlayerCharacter.cs
+++ csharp/RPG-Combat/PlayerCharacter.cs
@@ -1,14 +1,113 @@
 using System;
+using System.Collections.Generic;
 
 namespace rpg;
 
 public class PlayerCharacter
 {
-    public PlayerCharacter(string name)
-    {
+    private string name;
+    private int health;
+    private int level;
+    private bool alive;
+    HashSet<Faction> factions = new HashSet<Faction>();
+
+    public PlayerCharacter(string name) : this(name, 1000, 1, true) {
+    }
+    
+    public PlayerCharacter(string name, int health, int level, bool alive) {
+        this.name = name;
+        this.health = health;
+        this.level = level;
+        this.alive = alive;
     }
 
     public void receiveDamage(PlayerCharacter enemy, int damagePoints)
     {
+        if (this == enemy || sameFaction(enemy)) {
+            return;
+        }
+        if (this.level >= (enemy.level + 5)) {
+            this.health -= (int)Math.Round(damagePoints/2.0);
+        } else if (this.level <= (enemy.level -5)) {
+            this.health -= (int)(Math.Round(damagePoints*1.5));
+        } else {
+            this.health -= damagePoints;
+        }
+        if (this.health <= 0) {
+            this.health = 0;
+            this.alive = false;
+        }
+    }
+    
+    private bool sameFaction(PlayerCharacter otherCharacter) {
+        foreach (var faction in factions)
+        {
+            if (otherCharacter.factions.Contains(faction)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void heal(int damagePoints) {
+        if (!this.alive) {
+            return;
+        }
+        this.health += damagePoints;
+        if (this.health > 1000) {
+            this.health = 1000;
+        }
+    }
+
+    public void heal(PlayerCharacter healer, int points) {
+        if (sameFaction(healer)) {
+            this.heal(points);
+        }
+    }
+
+    public void joinFaction(Faction f) {
+        this.factions.Add(f);
+    }
+
+    public void leaveFaction(Faction f) {
+        this.factions.Remove(f);
+    }
+
+    public string PrintSimply()
+    {
+        return "PlayerCharacter{" +
+               "name='" + name + '\'' +
+               ", health=" + health +
+               ", level=" + level +
+               ", alive=" + alive +
+               '}';
+    }
+
+    public string PrintDetails()
+    {
+        return "PlayerCharacter{" +
+               "name='" + name + '\'' +
+               ", health=" + health +
+               ", level=" + level +
+               ", alive=" + alive +
+               ", factions=" + string.Join(", ", factions) +
+               '}';
+    }
+}
+
+
+public record Faction
+{
+    private readonly string name;
+
+    public Faction(string name)
+    {
+        this.name = name;
+    }
+
+    public override string ToString()
+    {
+        return name;
     }
 }
--- csharp/RPG-Combat/PlayerCharacterTest.cs
+++ csharp/RPG-Combat/PlayerCharacterTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
@@ -10,20 +11,141 @@ namespace rpg
     [UseReporter(typeof(QuietReporter))]
     public class PlayerCharacterTest
     {
+        private StringBuilder toVerify = new StringBuilder();
+
         [Fact]
         public void dealDamage()
         {
             // arrange
             var hero = new PlayerCharacter("Hero");
             var enemy = new PlayerCharacter("Orc");
-            var toVerify = "";
+            var toVerify = printCharacterBasics(new List<PlayerCharacter> { hero, enemy });;
 
             // act
-            toVerify += "Orc deals 100 damage to Hero\n";
+            toVerify += "\n\nOrc deals 100 damage to Hero\n\n";
             hero.receiveDamage(enemy, 100);
 
             // assert
-            toVerify += StringUtils.ToReadableString(new List<PlayerCharacter>{hero, enemy});
+            toVerify += printCharacterBasics(new List<PlayerCharacter> { hero, enemy });
+            Approvals.Verify(toVerify);
+        }
+
+        private string printCharacterBasics(List<PlayerCharacter> heroes)
+        {
+            var heroesPrintedSimply = new List<string>();
+            foreach (var hero in heroes)
+            {
+                heroesPrintedSimply.Add(hero.PrintSimply());
+            }
+
+            return string.Join("\n", heroesPrintedSimply);
+        }
+
+
+        [Fact]
+        void dealDamageFromPowerfulEnemy()
+        {
+            // arrange
+            List<PlayerCharacter> heroes = new List<PlayerCharacter>()
+            {
+                new PlayerCharacter("Hero", 900, 1, true),
+                new PlayerCharacter("Hero with max health", 1000, 1, true),
+                new PlayerCharacter("dead hero", 0, 1, false),
+                new PlayerCharacter("dying hero", 50, 1, true),
+                new PlayerCharacter("hero who is same level as orc", 101, 6, true),
+                new PlayerCharacter("hero who is 5 levels above orc", 51, 11, true)
+            };
+            var enemy = new PlayerCharacter("Orc", 1000, 6, true);
+            toVerify.Append(printCharacterBasics(heroes));
+
+            // act
+            toVerify.Append("\n\nLevel 6 Orc deals 100 damage to each Hero\n\n");
+            heroes.ForEach(c => c.receiveDamage(enemy, 100));
+
+            // assert
+            toVerify.Append(printCharacterBasics(heroes));
+            Approvals.Verify(toVerify);
+        }
+
+        [Fact]
+        void cannotDamageYourself()
+        {
+            var hero = new PlayerCharacter("Hero");
+            hero.receiveDamage(hero, 100);
+            toVerify.Append(hero.PrintSimply());
+            Approvals.Verify(toVerify);
+        }
+
+        [Fact]
+        void healing()
+        {
+            List<PlayerCharacter> heroes = new List<PlayerCharacter>()
+            {
+                new PlayerCharacter("Hero", 900, 1, true),
+                new PlayerCharacter("Hero with max health", 1000, 1, true),
+                new PlayerCharacter("dead hero", 0, 1, false)
+            };
+            toVerify.Append(printCharacterBasics(heroes));
+
+            // act
+            toVerify.Append("\n\nAll Heroes heal 100 damage\n\n");
+            heroes.ForEach(c => c.heal(100));
+
+            // assert
+            toVerify.Append(printCharacterBasics(heroes));
+            Approvals.Verify(toVerify);
+        }
+
+
+        [Fact]
+        void joinFactions()
+        {
+            var hero = new PlayerCharacter("Hero");
+            hero.joinFaction(new Faction("Blues"));
+            hero.joinFaction(new Faction("Reds"));
+            // duplicate addition has no effect
+            hero.joinFaction(new Faction("Reds"));
+            // join and leave a faction
+            hero.joinFaction(new Faction("Yellows"));
+            hero.leaveFaction(new Faction("Yellows"));
+            toVerify.Append(hero.PrintDetails());
+            Approvals.Verify(toVerify);
+        }
+
+        [Fact]
+        void receiveNoDamageFromSameFaction()
+        {
+            var hero1 = new PlayerCharacter("Blue Hero 1");
+            hero1.joinFaction(new Faction("Blues"));
+            var hero2 = new PlayerCharacter("Blue Hero 2");
+            hero2.joinFaction(new Faction("Blues"));
+            hero1.receiveDamage(hero2, 100);
+            toVerify.Append(hero1.PrintDetails());
+            Approvals.Verify(toVerify);
+        }
+
+        [Fact]
+        void sameFactionCanHealCharacter()
+        {
+            var healer = new PlayerCharacter("Blue Healer");
+            healer.joinFaction(new Faction("Blues"));
+
+            List<PlayerCharacter> heroes = new List<PlayerCharacter>()
+            {
+                new PlayerCharacter("Blue Hero 1 with max health"),
+                new PlayerCharacter("Blue slightly injured hero", 900, 1, true),
+                new PlayerCharacter("Blue dead hero", 0, 1, false)
+            };
+            heroes.ForEach(h => h.joinFaction(new Faction("Blues")));
+            heroes.Add(new PlayerCharacter("Neutral slightly injured hero", 900, 1, true));
+            toVerify.Append(printCharacterBasics(heroes));
+
+            // act
+            toVerify.Append("\n\nAll Heroes get healed 100 damage by Blue healer\n\n");
+            heroes.ForEach(c => c.heal(healer, 100));
+
+            // assert
+            toVerify.Append(printCharacterBasics(heroes));
             Approvals.Verify(toVerify);
         }
     }
