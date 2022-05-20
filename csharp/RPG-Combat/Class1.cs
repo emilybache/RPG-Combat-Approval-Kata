57d7f8f0652ada7bad139a650d4d7a5667b848f6
emily <emily@bacheconsulting.com>
2022-05-20 11:55:59 +0200

    initial translation

---
+++ csharp/RPG-Combat/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace rpg
+{
+    public class Class1
+    {
+    }
+}
---
+++ csharp/RPG-Combat/PlayerCharacter.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace rpg;
+
+public class PlayerCharacter
+{
+    public PlayerCharacter(string name)
+    {
+    }
+
+    public void receiveDamage(PlayerCharacter enemy, int damagePoints)
+    {
+    }
+}
