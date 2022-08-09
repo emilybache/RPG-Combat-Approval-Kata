ccf9d7ef1a511cdeae364553fd8bf28375c0aa39
Emily Bache <emily@bacheconsulting.com>
2022-08-09 14:56:40 +0000

    sample solution for damage and healing

--- python-texttest/src/rpg_combat.py
+++ python-texttest/src/rpg_combat.py
@@ -4,27 +4,89 @@ import json
 
 
 class DealDamage:
-	def __init__(self, attacker_name, defender_name,  amount):
-		self.attacker_name = attacker_name
-		self.defender_name = defender_name
-		self.damagePoints = amount
+    def __init__(self, attacker_name, defender_name, amount):
+        self.attacker_name = attacker_name
+        self.defender_name = defender_name
+        self.damagePoints = amount
 
-	def play(self, characters):
-		attacker = characters[self.attacker_name]
-		defender = characters[self.defender_name]
-		defender.receive_damage(attacker, self.damagePoints)
+    def play(self, characters):
+        attacker = characters[self.attacker_name]
+        defender = characters[self.defender_name]
+        defender.receive_damage(attacker, self.damagePoints)
+
+    def __str__(self):
+        return f"DealDamage: {self.damagePoints} damage points from attacker {self.attacker_name} on defender {self.defender_name}"
+
+
+class DealDamageOnMultipleCharacters:
+    def __init__(self, attacker_name, defender_names, amount):
+        self.attacker_name = attacker_name
+        self.defender_names = defender_names
+        self.damagePoints = amount
+
+    def play(self, characters):
+        attacker = characters[self.attacker_name]
+        defenders = [characters[name] for name in self.defender_names]
+        for defender in defenders:
+            defender.receive_damage(attacker, self.damagePoints)
+
+    def __str__(self):
+        return f"DealDamageOnMultipleCharacters: {self.damagePoints} damage points from attacker {self.attacker_name} on defenders {self.defender_names}"
+
+
+class HealingMultipleCharacters:
+    def __init__(self, character_names, amount):
+        self.amount = amount
+        self.character_names = character_names
+
+    def play(self, characters):
+        patients = [characters[name] for name in self.character_names]
+        for p in patients:
+            p.heal(self.amount)
+
+    def __str__(self):
+        return f"HealingMultipleCharacters: {self.amount} healing to {self.character_names}"
 
 
 class Character:
-	def __init__(self, name, health, isAlive):
-		self.name = name
-		self.health = health
-		self.isAlive = isAlive
+    def __init__(self, name, health=1000, isAlive=True, level=1, factions=None):
+        self.name = name
+        self.health = health
+        self.isAlive = isAlive
+        self.level = level
+        self.factions = factions or []
 
-	def receive_damage(self, attacker, damagePoints):
-		# TODO: implement this functionality
-		pass
+    def receive_damage(self, attacker, damagePoints):
+        if self == attacker or self.same_faction(attacker):
+            return
+
+        if self.level >= (attacker.level + 5):
+            self.health -= int(damagePoints / 2.0)
+
+        elif self.level <= (attacker.level - 5):
+            self.health -= int(damagePoints * 1.5)
+
+        else:
+            self.health -= damagePoints
+
+        if (self.health <= 0):
+            self.health = 0
+            self.isAlive = False
+
+    def heal(self, damagePoints):
+        if not self.isAlive:
+            return
+
+        self.health += damagePoints
+        if self.health > 1000:
+            self.health = 1000
+
+    def same_faction(self, character):
+        for faction in self.factions:
+            if faction in character.factions:
+                return True
+        return False
 
 
 def play(characters, move):
-	move.play(characters)
+    move.play(characters)
--- python-texttest/test/test_rig.py
+++ python-texttest/test/test_rig.py
@@ -5,58 +5,67 @@ import json
 
 
 def print_character(character):
-	"This is the Printer you need to implement so that you can see what's happened to the Character during combat"
-	return str(character)
+    "This is the Printer you need to implement so that you can see what's happened to the Character during combat"
+    return f"Character(name='{character.name}'" \
+           f", health={character.health}" \
+           f", level={character.level}" \
+           f", alive={character.isAlive}" \
+           ")"
 
 
 def print_move(move):
-	"This is the Printer you need to implement so that you can see what moves happen during combat"
-	return str(move)
+    "This is the Printer you need to implement so that you can see what moves happen during combat"
+    return str(move)
 
 
 def init_characters(characters_as_json):
-	characters = json.loads(characters_as_json, object_hook=lambda c: rpg_combat.Character(**c))
-	return dict((character.name, character) for character in characters)
+    characters = json.loads(characters_as_json, object_hook=lambda c: rpg_combat.Character(**c))
+    return dict((character.name, character) for character in characters)
 
 
 def init_moves(moves_as_json):
-	moves = []
-	raw_moves = json.loads(moves_as_json)
-	for move in raw_moves:
-		if move["moveType"] == "DealDamage":
-			moves.append(rpg_combat.DealDamage(move["character1"], move["character2"], move["amount"]))
-	return moves
+    moves = []
+    raw_moves = json.loads(moves_as_json)
+    for move in raw_moves:
+        moveType = move["moveType"]
+        if moveType == "DealDamage":
+            moves.append(rpg_combat.DealDamage(move["character1"], move["character2"], move["amount"]))
+        elif moveType == "DealDamageOnMultipleCharacters":
+            moves.append(rpg_combat.DealDamageOnMultipleCharacters(move["attacker"], move["defenders"], move["amount"]))
+        elif moveType == "HealingMultipleCharacters":
+            moves.append(rpg_combat.HealingMultipleCharacters(move["patients"], move["amount"]))
+    return moves
 
 
 def main():
-	# Arrange
-	with open("characters.json") as f:
-		characters_as_json = f.read()
+    # Arrange
+    with open("characters.json") as f:
+        characters_as_json = f.read()
 
-	with open("combat_moves.json") as f:
-		combat_moves_as_json = f.read()
+    with open("combat_moves.json") as f:
+        combat_moves_as_json = f.read()
 
-	characters = init_characters(characters_as_json)
-	print_characters(characters)
+    characters = init_characters(characters_as_json)
+    print_characters(characters)
 
-	moves = init_moves(combat_moves_as_json)
+    moves = init_moves(combat_moves_as_json)
 
-	# Act
-	print("")
-	for move in moves:
-		print(f"playing move {print_move(move)}")
-		rpg_combat.play(characters, move)
+    # Act
+    print("")
+    for move in moves:
+        print(f"playing move {print_move(move)}")
+        rpg_combat.play(characters, move)
 
-	# Assert
-	print_characters(characters)
+    # Assert
+    print_characters(characters)
 
 
 def print_characters(characters):
-	print("")
-	print("Characters:")
-	for name, character in list(sorted(characters.items())):
-		print(f"    {print_character(character)}")
+    print("")
+    print("Characters:")
+    for name, character in list(sorted(characters.items())):
+        print(f"    {print_character(character)}")
 
 
 if __name__ == "__main__":
-	main()
+    main()
