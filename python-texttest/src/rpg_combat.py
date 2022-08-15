181944289d44faec11690c74b1c72d56c9aa852e
Emily Bache <emily@bacheconsulting.com>
2022-08-15 08:00:15 +0000

    after feedback

--- python-texttest/src/rpg_combat.py
+++ python-texttest/src/rpg_combat.py
@@ -3,28 +3,35 @@
 import json
 
 
-class DealDamage:
-	def __init__(self, attacker_name, defender_name,  amount):
-		self.attacker_name = attacker_name
-		self.defender_name = defender_name
-		self.damagePoints = amount
+class Move:
+    "abstract interface"
 
-	def play(self, characters):
-		attacker = characters[self.attacker_name]
-		defender = characters[self.defender_name]
-		defender.receive_damage(attacker, self.damagePoints)
+    def play(self, characters):
+        pass
+
+
+class DealDamage(Move):
+    def __init__(self, attacker_name, defender_name, amount):
+        self.attacker_name = attacker_name
+        self.defender_name = defender_name
+        self.damagePoints = amount
+
+    def play(self, characters):
+        attacker = characters[self.attacker_name]
+        defender = characters[self.defender_name]
+        defender.receive_damage(attacker, self.damagePoints)
 
 
 class Character:
-	def __init__(self, name, health, isAlive):
-		self.name = name
-		self.health = health
-		self.isAlive = isAlive
+    def __init__(self, name, health, isAlive):
+        self.name = name
+        self.health = health
+        self.isAlive = isAlive
 
-	def receive_damage(self, attacker, damagePoints):
-		# TODO: implement this functionality
-		pass
+    def receive_damage(self, attacker: Character, damagePoints: int):
+        # TODO: implement this functionality
+        pass
 
 
 def play(characters, move):
-	move.play(characters)
+    move.play(characters)
