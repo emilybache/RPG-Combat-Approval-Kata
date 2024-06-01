8d438bfd5e9805aa740f55e89d1bc7720e8bbd2d
Thomas Traude <thomas.traude@codecentric.de>
2024-06-01 18:03:33 +0200

    [java] Remove obsolete public modifier in test

--- java/src/test/java/codingdojo/PlayerCharacterTest.java
+++ java/src/test/java/codingdojo/PlayerCharacterTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PlayerCharacterTest {
+class PlayerCharacterTest {
     @Test
     void dealDamage() {
         // arrange
